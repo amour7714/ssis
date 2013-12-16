20c27d2596796103a106fd3f1bc434353609ea22
Grant Schulte <gschulte@arrow.ca>
2013-12-16 13:36:25 -0800

    dw: added process.start_Task_sp

---
+++ ArrowDW/ArrowDW/sql/process.start_Task_sp.sql
@@ -0,0 +1,50 @@
+USE arrowdw
+go
+
+
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'process.start_Task_sp') AND type in (N'P', N'PC'))
+	DROP PROCEDURE process.start_Task_sp
+go
+
+
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/********************************************************************************
+-- Author:		Grant Schulte
+-- Create date: 2013-12-16
+-- Description:	Creates a new task in process.Task from the parameters passed in 
+*********************************************************************************/
+
+CREATE PROCEDURE process.start_Task_sp
+	@BatchID int,
+	@TaskDescript varchar(100)
+AS
+BEGIN
+
+	SET NOCOUNT ON;
+
+	INSERT INTO [process].[Task]
+           ([BatchID]
+           ,[TaskDescript]
+           ,[StartDateTime]
+           ,[EndDateTime]
+           ,[TaskStatus]
+           ,[NumRowsInserted]
+           ,[NumRowsUpdated]
+           ,[ErrorMessage])
+     VALUES
+           (@BatchID
+           ,@TaskDescript
+           ,getdate()
+           ,NULL
+           ,'r' --running
+           ,0
+           ,0
+           ,'')
+
+END
+GO
+
