ff5e112aaec14b75fd6ceeedeb032efe59f4f985
Grant Schulte <gschulte@arrow.ca>
2014-01-02 08:17:20 -0800

    cp: fixed bug in building @14_months_ago variable date string

--- customerportal/customerportal/sql/inventory.rpt_custportal_completed_orders_staging_sp.sql
+++ customerportal/customerportal/sql/inventory.rpt_custportal_completed_orders_staging_sp.sql
@@ -14,14 +14,14 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
--- ==================================================================================================
+-- ========================================================================================================================
 -- Author:		Grant Schulte
 -- Create date: 2013-07-15
 -- Description: Stages data for use by the Completed Orders chart on the customer portal dashboard
--- Updated by:	
--- Update Date: 
--- Update Description: 
--- ==================================================================================================
+-- Updated by:	gschulte
+-- Update Date: 2014-01-01
+-- Update Description: Fixed bug where the month portion of the @14_months_ago variable would be set to '00' in January
+-- ========================================================================================================================
 
 CREATE PROCEDURE [inventory].[rpt_custportal_completed_orders_staging_sp]
 AS
@@ -29,9 +29,20 @@ BEGIN
 
 	SET NOCOUNT ON;
 
-	declare @14_months_ago datetime;
+	--declare @14_months_ago datetime;
 
-	set @14_months_ago = convert(datetime, convert(varchar(4), year(getdate())-1) + '-' + right('0' + convert(varchar(2), month(getdate())-1), 2) + '-01')
+	--set @14_months_ago = convert(datetime, convert(varchar(4), year(getdate())-1) + '-' + right('0' + isnull(nullif(convert(varchar(2), month(getdate())-1), 0), 12), 2) + '-01')
+
+	declare @14_months_ago datetime,
+			@now datetime = getdate(),
+			@y int;
+
+	set @y = case month(@now)
+				when 1 then year(@now)-2
+				else year(@now)-1
+			 end
+
+	set @14_months_ago = convert(datetime, convert(varchar(4), @y) + '-' + right('0' + isnull(nullif(convert(varchar(2), month(@now)-1), 0), 12), 2) + '-01')
 
 	select o.customer_id,
 		   d.branch_code,
